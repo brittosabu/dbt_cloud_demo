89274369767c8d4b56745c9b36b839d92d2fe21b
Britto Sabu <brittosabu07@gmail.com>
2024-01-04 02:43:41 +0000

    using sources

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,7 +3,7 @@ SELECT
     id as customer_id,
     first_name,
     last_name
-FROM `dbt-tutorial`.jaffle_shop.customers
+FROM {{ source('jaffle_shop', 'customers') }}
 )
 
 SELECT * 
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,7 +4,7 @@ SELECT
     user_id as customer_id,
     order_date,
     status
-FROM `dbt-tutorial`.jaffle_shop.orders
+FROM {{ source('jaffle_shop', 'orders') }}
 )
 
 SELECT * 
